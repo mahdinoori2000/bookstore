ad8f62e58780ebdf0b36f8d348ed2d20a177cbaf
Mahdi Noori <mahdinoori2389@gmail.com>
2023-07-29 16:57:13 +0430

    Add axios

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,34 +1,66 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
+  loading: false,
   books: [],
+  error: '',
 };
 
+// const uniqueId = 'NMdtmp7Xf82moZUrly0Y';
+const apiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/NMdtmp7Xf82moZUrly0Y/books/';
+
+const fetchBooks = createAsyncThunk('books/fetchbooks', async () => {
+  const response = await axios.get(apiUrl);
+  const data = response;
+  return ('data', data);
+});
+console.log(fetchBooks());
 const booksSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {
-    addBook: {
-      prepare(id, title, author) {
-        return { payload: { id, title, author } };
-      },
-      reducer(state, action) {
-        state.books.push({
-          item_id: action.payload.id,
-          title: action.payload.title,
-          author: action.payload.author,
-          catagory: '',
-        });
-      },
-    },
-    removeBook: (state, action) => (
-      {
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => ({
         ...state,
-        books: state.books.filter((book) => book.item_id !== action.payload),
-      }
-    ),
+        loading: true,
+      }))
+      .addCase(fetchBooks.fulfilled, (state, action) => ({
+        ...state,
+        loading: false,
+        books: action.payload,
+      }))
+      .addCase(fetchBooks.rejected, (state, action) => ({
+        ...state,
+        error: action.error.message,
+      }))
   },
+
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
 export default booksSlice.reducer;
+
+
+
+// reducers: {
+//   addBook: {
+//     prepare(id, title, author) {
+//       return { payload: { id, title, author } };
+//     },
+//     reducer(state, action) {
+//       state.books.push({
+//         item_id: action.payload.id,
+//         title: action.payload.title,
+//         author: action.payload.author,
+//         catagory: '',
+//       });
+//     },
+//   },
+//   removeBook: (state, action) => (
+//     {
+//       ...state,
+//       books: state.books.filter((book) => book.item_id !== action.payload),
+//     }
+//   ),
+// },
